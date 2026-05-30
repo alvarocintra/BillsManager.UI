ac60a0921556115862c800e608a11b120017a9b6
alvarocintra <alvarocintra21@gmail.com>
2026-05-30 12:22:52 +0100

    render mode server

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,10 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // Exclui do prerender as rotas com parâmetros
+  { path: 'categories/:id', renderMode: RenderMode.Server },
+  { path: 'bills/:id', renderMode: RenderMode.Server },
+  // As restantes continuam a ser pré-renderizadas
   {
     path: '**',
     renderMode: RenderMode.Prerender
