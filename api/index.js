778c40e324c661688d35ae6a863253a91b154bdb
alvarocintra <alvarocintra21@gmail.com>
2026-06-14 20:45:50 +0100

    fix

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 export default async function handler(req, res) {
-  const { reqHandler } = await import('../dist/BillsManager.UI/server/main.server.mjs');
+  const { reqHandler } = await import('../dist/BillsManager.UI/server/server.mjs');
   return reqHandler(req, res);
 }
