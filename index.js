f85c1070aee61e65e35f24735d0139a48e138757
alvarocintra <alvarocintra21@gmail.com>
2026-06-14 21:34:57 +0100

    try/catch

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 
 export default async function handler(req, res) {
-  const { reqHandler } = await import('./dist/BillsManager.UI/server/server.mjs');
-  return reqHandler(req, res);
+    try {
+        const { reqHandler } = await import('./dist/BillsManager.UI/server/server.mjs');
+        return reqHandler(req, res);
+    } catch (error) {
+        console.error('Error occurred while handling request:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 }
